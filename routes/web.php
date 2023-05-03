b23b9f0a6d243e2a971622b7b3dc9eccd21fad0e
aslan-asilon3 <sulaslansetiawan3@gmail.com>
2023-05-02 23:52:10 -0700

    update registration

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,15 @@ use App\Http\Controllers\Auth\LoginController;
 //     return view('welcome');
 // });
 
-Auth::routes();
+// Auth::routes();
+Auth::routes([
+    'register' => false, // Registration Routes...
+    'reset' => false, // Password Reset Routes...
+    'verify' => false, // Email Verification Routes...
+  ]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
-
 
 Route::resource('users', UserController::class);
 Route::resource('projects', ProjectController::class);
